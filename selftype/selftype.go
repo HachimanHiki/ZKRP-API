e38b087608eedf0a607fb179aab7df1746ae9ad2
HachimanHiki <mh512158475@gmail.com>
2020-06-27 13:34:08 +0800

    update Event type

--- selftype/selftype.go
+++ selftype/selftype.go
@@ -1,7 +1,7 @@
 package selftype
 
 type DiseaseInfo struct {
-	DiseaseID string `json:"diseaseId" example:"R093"`
+	DiseaseID string `json:"diseaseId" example:"R"`
 	DiseaseName string `json:"diseaseName" example:"\u75f0\u7570\u5e38"`
 	RequireDays int `json:"requireDays" example:"28"`
 }
@@ -55,6 +55,8 @@ type Verify struct {
 
 type Event struct {
 	EventName string `json:"eventName" example:"marathon"`
+	EventInfo string `json:"eventInfo" example:"強化主動防疫 我們需要您提供28天內住院與手術資料\n\n我們將從您的健康存摺加密處理後交給馬拉松中心進行第三方驗證 確定您是否符合馬拉松資格"`
+	EventType string `json:"EventType" example:"zkrp"`
 	DiseaseInfo []DiseaseInfo `json:"diseaseInfo"`
 	OperationInfo []OperationInfo `json:"operationInfo"`
 	VarifyURL string `json:"varifyURL" example:"http://localhost:8080/verify"`
